615eee3a1d76cb8fdd26c4d16428612e3a3792c2
InhyeKang <kanginhae0826@gmail.com>
2023-09-21 19:17:01 +0900

    ⚙️ :: 라이브러리 수정 및 폰트 추가

--- Projects/Feature/ApplyFeature/Sources/ApplyVC.swift
+++ Projects/Feature/ApplyFeature/Sources/ApplyVC.swift
@@ -1,6 +1,7 @@
 import BaseFeature
 import UIKit
+import SnapKit
+import Then
 
 public class ApplyVC: BaseVC {
-
 }
--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -8,9 +8,9 @@ let dependencies = Dependencies(
             [
                 .remote(url: "https://github.com/Quick/Quick.git", requirement: .upToNextMajor(from: "5.0.1")),
                 .remote(url: "https://github.com/Quick/Nimble.git", requirement: .upToNextMajor(from: "10.0.0")),
-                .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.1")),
+                .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.0.0")),
                 .remote(url: "https://github.com/devxoul/Then.git", requirement: .upToNextMajor(from: "3.0.0")),
-                .remote(url: "https://github.com/realm/realm-cocoa.git", requirement: .upToNextMajor(from: "10.0.0"))
+                .remote(url: "https://github.com/realm/realm-swift.git", requirement: .upToNextMajor(from: "10.42.3"))
             ],
             baseSettings: .settings(
                 configurations: [
