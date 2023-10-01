b743320199b59f08bf0b7cecf9147b7ab985999c
InhyeKang <kanginhae0826@gmail.com>
2023-10-02 01:27:26 +0900

    ⚡️ :: DesignSystem Image 기능 추가

---
+++ Projects/Shared/DesignSystem/Sources/Image/Image.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+public extension UIImage {
+    enum Image {
+        public static let calendar = DesignSystemAsset.ImageAssets.calendar.image
+        public static let logo = DesignSystemAsset.ImageAssets.yuseongLogo.image
+        public static let invisible = DesignSystemAsset.ImageAssets.invisible.image
+    }
+}
