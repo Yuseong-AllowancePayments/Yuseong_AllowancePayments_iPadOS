9af2705b58cb5def20820ec2eccf9c0f3ffe8f4a
juyeong525 <juyeongno1@naver.com>
2023-10-09 14:30:56 +0900

    ⚡️ :: AppStepper

---
+++ Projects/App/Sources/App/Flow/AppStepper.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+import RxCocoa
+import RxFlow
+import RxSwift
+
+class AppStepper: Stepper {
+
+    let steps = PublishRelay<Step>()
+    private let disposeBag = DisposeBag()
+
+    init() {}
+
+    func readyToEmitSteps() {
+        Observable.just(YuseongAllowanceStep.selectTypeIsRequired)
+            .delay(RxTimeInterval.seconds(2), scheduler: MainScheduler.instance)
+            .bind(to: steps)
+            .disposed(by: disposeBag)
+    }
+}
