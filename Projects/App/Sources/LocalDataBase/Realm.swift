1b6d910bc211b2605e0afbc7c6679e89f04dfe7c
InhyeKang <kanginhae0826@gmail.com>
2023-12-18 09:14:39 +0900

    ⚡️ :: Table 추가

--- Projects/App/Sources/LocalDataBase/Realm.swift
+++ Projects/App/Sources/LocalDataBase/Realm.swift
@@ -8,27 +8,27 @@ final class LocalData: Object {
 }
 
 class VeteranTable: Object {
-    @Persisted var paymentTargetTab: List<PaymentTargetTab>
-    @Persisted var cashPaymentTab: List<CashPaymentTab>
-    @Persisted var newComerVeteranTab: List<NewcomerVeteranTab>
-    @Persisted var stoppageTab: List<StoppageTab>
+    @Persisted var paymentTargetTab: List<VeteranPaymentTargetTab>
+    @Persisted var cashPaymentTab: List<VeteranCashPaymentTab>
+    @Persisted var newComerVeteranTab: List<VeteranNewcomerTab>
+    @Persisted var stoppageTab: List<VeteranStoppageTab>
 }
 
 class SpouseTable: Object {
-    @Persisted var paymentTargetTab: List<PaymentTargetTab>
-    @Persisted var cashpaymentTab: List<CashPaymentTab>
-    @Persisted var newComerSpouseTab: List<NewcomerSpouseTab>
-    @Persisted var stoppageTab: List<StoppageTab>
+    @Persisted var paymentTargetTab: List<SpousePaymentTargetTab>
+    @Persisted var cashpaymentTab: List<SpouseCashPaymentTab>
+    @Persisted var newComerSpouseTab: List<SpouseNewcomerTab>
+    @Persisted var stoppageTab: List<SpouseStoppageTab>
 }
 
 class CourtesyTable: Object {
-    @Persisted var paymentTargetTab: List<PaymentTargetTab>
-    @Persisted var cashpaymentTab: List<CashPaymentTab>
-    @Persisted var newComerCourtesyTab: List<NewcomerCourtesyTab>
-    @Persisted var stoppageTab: List<StoppageTab>
+    @Persisted var paymentTargetTab: List<CourtesyPaymentTargetTab>
+    @Persisted var cashpaymentTab: List<CourtesyCashPaymentTab>
+    @Persisted var newComerCourtesyTab: List<CourtesyNewcomerTab>
+    @Persisted var stoppageTab: List<CourtesyStoppageTab>
 }
 
-class PaymentTargetTab: Object {
+class VeteranPaymentTargetTab: Object {
     @Persisted var serial_num: String = ""
     @Persisted var administrative_address: String = ""
     @Persisted var affairs_num: String = ""
@@ -44,7 +44,7 @@ class PaymentTargetTab: Object {
     @Persisted var note: String = ""
 }
 
-class CashPaymentTab: Object {
+class VeteranCashPaymentTab: Object {
     @Persisted var serial_num: String = ""
     @Persisted var administrative_address: String = ""
     @Persisted var affairs_num: String = ""
@@ -57,24 +57,7 @@ class CashPaymentTab: Object {
     @Persisted var note: String = ""
 }
 
-class StoppageTab: Object {
-    @Persisted var serial_num: String = ""
-    @Persisted var administrative_address: String = ""
-    @Persisted var affairs_num: String = ""
-    @Persisted(primaryKey: true) var sin: String = ""
-    @Persisted var name: String = ""
-    @Persisted var address: String = ""
-    @Persisted var deposit_type: String = ""
-    @Persisted var bank_name: String = ""
-    @Persisted var account_owner: String = ""
-    @Persisted var account: String = ""
-    @Persisted var note: String = ""
-    @Persisted var stoppage_reason: String = ""
-    @Persisted var stoppage_date: String = ""
-    @Persisted var move_in_address: String = ""
-}
-
-class NewcomerVeteranTab: Object {
+class VeteranNewcomerTab: Object {
     @Persisted var serial_num: String = ""
     @Persisted var name: String = ""
     @Persisted var birth_date: String = ""
@@ -93,29 +76,121 @@ class NewcomerVeteranTab: Object {
     @Persisted var note: String = ""
 }
 
-class NewcomerSpouseTab: Object {
-    @Persisted var serial_num: String
-    @Persisted var applicant_name: String
-    @Persisted var applicant_sin: String
-    @Persisted var applicant_phone_num: String
-    @Persisted var applicant_post_address: String
-    @Persisted var applicant_road_address: String
-    @Persisted var administrative_address: String
-    @Persisted var veteran_name: String
-    @Persisted var war_name: String
-    @Persisted var veteran_sin: String
-    @Persisted(primaryKey: true) var affairs_num: String
-    @Persisted var death_date: String
-    @Persisted var bank_name: String
-    @Persisted var account_owner: String
-    @Persisted var account: String
-    @Persisted var move_in_date: String
-    @Persisted var application_date: String
-    @Persisted var application_reason: String
-    @Persisted var note: String
+class VeteranStoppageTab: Object {
+    @Persisted var serial_num: String = ""
+    @Persisted var administrative_address: String = ""
+    @Persisted var affairs_num: String = ""
+    @Persisted(primaryKey: true) var sin: String = ""
+    @Persisted var name: String = ""
+    @Persisted var address: String = ""
+    @Persisted var deposit_type: String = ""
+    @Persisted var bank_name: String = ""
+    @Persisted var account_owner: String = ""
+    @Persisted var account: String = ""
+    @Persisted var note: String = ""
+    @Persisted var stoppage_reason: String = ""
+    @Persisted var stoppage_date: String = ""
+    @Persisted var move_in_address: String = ""
 }
 
-class NewcomerCourtesyTab: Object {
+class SpousePaymentTargetTab: Object {
+    @Persisted var serial_num: String = ""
+    @Persisted var administrative_address: String = ""
+    @Persisted var affairs_num: String = ""
+    @Persisted(primaryKey: true) var sin: String = ""
+    @Persisted var name: String = ""
+    @Persisted var address: String = ""
+    @Persisted var deposit_type: String = ""
+    @Persisted var bank_name: String = ""
+    @Persisted var account_owner: String = ""
+    @Persisted var account: String = ""
+    @Persisted var sibi: String = ""
+    @Persisted var gubi: String = ""
+    @Persisted var note: String = ""
+}
+
+class SpouseCashPaymentTab: Object {
+    @Persisted var serial_num: String = ""
+    @Persisted var administrative_address: String = ""
+    @Persisted var affairs_num: String = ""
+    @Persisted(primaryKey: true) var sin: String = ""
+    @Persisted var name: String = ""
+    @Persisted var address: String = ""
+    @Persisted var deposit_type: String = ""
+    @Persisted var sibi: String = ""
+    @Persisted var gubi: String = ""
+    @Persisted var note: String = ""
+}
+
+class SpouseNewcomerTab: Object {
+    @Persisted var serial_num: String = ""
+    @Persisted var applicant_name: String = ""
+    @Persisted var applicant_sin: String = ""
+    @Persisted var applicant_phone_num: String = ""
+    @Persisted var applicant_post_address: String = ""
+    @Persisted var applicant_road_address: String = ""
+    @Persisted var administrative_address: String = ""
+    @Persisted var veteran_name: String = ""
+    @Persisted var war_name: String = ""
+    @Persisted var veteran_sin: String = ""
+    @Persisted(primaryKey: true) var affairs_num: String = ""
+    @Persisted var death_date: String = ""
+    @Persisted var bank_name: String = ""
+    @Persisted var account_owner: String = ""
+    @Persisted var account: String = ""
+    @Persisted var move_in_date: String = ""
+    @Persisted var application_date: String = ""
+    @Persisted var application_reason: String = ""
+    @Persisted var note: String = ""
+}
+
+class SpouseStoppageTab: Object {
+    @Persisted var serial_num: String = ""
+    @Persisted var administrative_address: String = ""
+    @Persisted var affairs_num: String = ""
+    @Persisted(primaryKey: true) var sin: String = ""
+    @Persisted var name: String = ""
+    @Persisted var address: String = ""
+    @Persisted var deposit_type: String = ""
+    @Persisted var bank_name: String = ""
+    @Persisted var account_owner: String = ""
+    @Persisted var account: String = ""
+    @Persisted var note: String = ""
+    @Persisted var stoppage_reason: String = ""
+    @Persisted var stoppage_date: String = ""
+    @Persisted var move_in_address: String = ""
+}
+
+class CourtesyPaymentTargetTab: Object {
+    @Persisted var serial_num: String = ""
+    @Persisted var administrative_address: String = ""
+    @Persisted var affairs_num: String = ""
+    @Persisted(primaryKey: true) var sin: String = ""
+    @Persisted var name: String = ""
+    @Persisted var address: String = ""
+    @Persisted var deposit_type: String = ""
+    @Persisted var bank_name: String = ""
+    @Persisted var account_owner: String = ""
+    @Persisted var account: String = ""
+    @Persisted var sibi: String = ""
+    @Persisted var gubi: String = ""
+    @Persisted var note: String = ""
+}
+
+class CourtesyCashPaymentTab: Object {
+    @Persisted var serial_num: String = ""
+    @Persisted var administrative_address: String = ""
+    @Persisted var affairs_num: String = ""
+    @Persisted(primaryKey: true) var sin: String = ""
+    @Persisted var name: String = ""
+    @Persisted var address: String = ""
+    @Persisted var deposit_type: String = ""
+    @Persisted var sibi: String = ""
+    @Persisted var gubi: String = ""
+    @Persisted var note: String = ""
+}
+
+class CourtesyNewcomerTab: Object {
     @Persisted var serial_num: String = ""
     @Persisted(primaryKey: true) var veteran_type: String = ""
     @Persisted var affairs_num: String = ""
@@ -136,4 +211,21 @@ class NewcomerCourtesyTab: Object {
     @Persisted var application_date: String = ""
     @Persisted var application_reason: String = ""
     @Persisted var note: String = ""
+}
+
+class CourtesyStoppageTab: Object {
+    @Persisted var serial_num: String = ""
+    @Persisted var administrative_address: String = ""
+    @Persisted var affairs_num: String = ""
+    @Persisted(primaryKey: true) var sin: String = ""
+    @Persisted var name: String = ""
+    @Persisted var address: String = ""
+    @Persisted var deposit_type: String = ""
+    @Persisted var bank_name: String = ""
+    @Persisted var account_owner: String = ""
+    @Persisted var account: String = ""
+    @Persisted var note: String = ""
+    @Persisted var stoppage_reason: String = ""
+    @Persisted var stoppage_date: String = ""
+    @Persisted var move_in_address: String = ""
 }
