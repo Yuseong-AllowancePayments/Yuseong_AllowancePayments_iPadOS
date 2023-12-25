10049649e6aec6aa81a8b999d4c65f6ab93a5351
InhyeKang <kanginhae0826@gmail.com>
2023-12-25 23:58:13 +0900

    ⚡️ :: webView connect

--- Projects/App/Sources/Presentation/Scene/ApplicationForm/VeteranApplyViewController.swift
+++ Projects/App/Sources/Presentation/Scene/ApplicationForm/VeteranApplyViewController.swift
@@ -97,9 +97,17 @@ class VeteranApplyViewController: BaseVC<ApplyViewModel> {
         $0.titleLabel?.font = .pretendard(.p2)
         $0.layer.cornerRadius = 8
     }
+    private let contentController = WKUserContentController()
+    private let configuration = WKWebViewConfiguration()
+    private let preferences = WKPreferences()
+    private var webView: WKWebView!
     override func bind() {
         let input = ApplyViewModel.Input(backButtonDidTap: backButton.rx.tap.asSignal())
         _ = viewModel.transform(input)
+        findAddressButton.rx.tap
+            .subscribe(onNext: { [self] in
+                createdWebView()
+            }).disposed(by: disposeBag)
     }
     override func addView() {
         view.addSubview(scrollView)
@@ -124,6 +132,8 @@ class VeteranApplyViewController: BaseVC<ApplyViewModel> {
     }
     override func configureVC() {
         self.hideKeyboardWhenTappedAround()
+//        DispatchQueue.main.async { [self] in
+//        }
     }
     // swiftlint:disable function_body_length
     override func setLayout() {
@@ -224,13 +234,36 @@ class VeteranApplyViewController: BaseVC<ApplyViewModel> {
     // swiftlint:enable function_body_length
 }
 
-//extension UIViewController {
-//    func callWebView() {
-//        let contentController = WKUserContentController()
-//        let configuration = WKWebViewConfiguration()
-//
-//        contentController.add(self, name: "")
-//        configuration.userContentController = contentController
-//        webView = WKWebView(frame: .zero, configuration: configuration)
-//    }
-//}
+extension VeteranApplyViewController: WKScriptMessageHandler, WKUIDelegate, WKNavigationDelegate {
+    func createdWebView() {
+        DispatchQueue.main.async { [self] in
+//            if #available(iOS 14.0, *) {
+//                configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+//            } else {
+//                configuration.preferences.javaScriptEnabled = true
+//            }
+//            preferences.javaScriptCanOpenWindowsAutomatically = true
+//            configuration.preferences = preferences
+            contentController.add(self, name: "iOS")
+            configuration.userContentController = contentController
+            webView = WKWebView(frame: .zero, configuration: configuration)
+
+//            webView.uiDelegate = self
+            webView.navigationDelegate = self
+//            webView.allowsBackForwardNavigationGestures = true
+
+            backView.addSubview(webView)
+            webView.snp.makeConstraints {
+                $0.top.bottom.equalToSuperview().inset(200)
+                $0.left.right.equalToSuperview().inset(150)
+            }
+
+            webView.load(URLRequest(url: URL(string: "https://daum-address-webview.vercel.app/")!))
+        }
+    }
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print(message.name)
+//        guard message.name == "iOS", let data = message.body as? [String: Any] else { return }
+//        print(data)
+    }
+}
